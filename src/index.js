b92fb9a45f40442dff62b1d025692eadcda2116b
Orflo <lual.antonelli@gmail.com>
2024-02-06 16:40:59 +0100

    changes3

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.get('/', (req,res) => {
 const mysqlConnection = mysql.createConnection({
     host: process.env.MYSQLDB_HOST,
     user: process.env.MYSQLDB_USER,
-    password: process.env.MYSQLDB_PASSWORD,
+    password: process.env.MYSQLDB_ROOT_PASSWORD,
     database: process.env.MYSQLDB_NAME,
     multipleStatements: true
 });
