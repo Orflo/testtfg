de8ade00e80ce1bc2fcde91dd3ccb90fd243b1ad
Orflo <lual.antonelli@gmail.com>
2024-02-06 17:08:01 +0100

    changes3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 const express = require('express');
-const mysql = require('mysql2');
+const { createPool } = require('mysql2/promise');
 const { config } = require('dotenv');
+
 config();
-
 const app = express();
+const mysqlConnection = createPool = ({
+    host: process.env.MYSQLDB_HOST,
+    user: process.env.MYSQLDB_USER,
+    password: process.env.MYSQLDB_ROOT_PASSWORD,
+    database: process.env.MYSQLDB_DATABASE,
+});
 
-const PORT = process.env.NODE_DOCKER_PORT || 3000;
+const PORT = process.env.NODE_DOCKER_PORT
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
 });
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
 });
 
-const mysqlConnection = mysql.createConnection({
-    host: process.env.MYSQLDB_HOST,
-    user: process.env.MYSQLDB_USER,
-    password: process.env.MYSQLDB_ROOT_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE,
-    multipleStatements: true
-});
-
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('Conexión con la base de datos correcta...');
-    else
-        console.log('¡Conexión fallida!' + JSON.stringify(err, undefined, 2));
+app.get('/ping', async (req, res) => {
+    const result = await mysqlConnection.query('SELECT NOW()')
+    res.json(result[0])
 });
