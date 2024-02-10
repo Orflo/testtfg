f96d3a8123e1bc96f87d61864d736a022af410d5
Orflo <lual.antonelli@gmail.com>
2024-02-10 17:58:08 +0100

    conRes

--- src/index.js
+++ src/index.js
@@ -4,26 +4,35 @@ const { config } = require('dotenv');
 
 config();
 const app = express();
-const mysqlConnection = mysql.createConnection({
-    host: process.env.MYSQLDB_HOST,
-    user: process.env.MYSQLDB_USER,
-    password: process.env.MYSQLDB_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE,
-    port: process.env.MYSQLDB_DOCKER_PORT
-});
+function connectToDatabase() {
+    const mysqlConnection = mysql.createConnection({
+        host: process.env.MYSQLDB_HOST,
+        user: process.env.MYSQLDB_USER,
+        password: process.env.MYSQLDB_PASSWORD,
+        database: process.env.MYSQLDB_DATABASE,
+        port: process.env.MYSQLDB_DOCKER_PORT
+    });
 
-const PORT = process.env.NODE_DOCKER_PORT
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`)
-});
+    mysqlConnection.connect((err) => {
+        if (err) {
+            console.log('¡Conexión fallida!' + JSON.stringify(err, undefined, 2));
+            setTimeout(connectToDatabase, 5000);
+        } else {
+            console.log('Conexión con la base de datos establecida.');
+            startServer();
+        }
+    });
+}
 
-app.get('/', (req,res) => {
-    res.send('Hello World!')
-});
+function startServer() {
+    const PORT = process.env.NODE_DOCKER_PORT
+    app.listen(PORT, () => {
+        console.log(`Servidor iniciado en el puerto ${PORT}`)
+    });
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('Conexión con la base de datos correcta...');
-    else
-        console.log('¡Conexión fallida!' + JSON.stringify(err, undefined, 2));
-});
+    app.get('/', (req,res) => {
+        res.send('Hello World!')
+    });
+}
+
+connectToDatabase();
