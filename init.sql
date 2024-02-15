3042d0582ae0976e25ea930417109a1d29c97846
Orflo <lual.antonelli@gmail.com>
2024-02-15 11:33:02 +0100

    dbChanges

---
+++ init.sql
@@ -0,0 +1,13 @@
+-- Crear la base de datos si no existe
+CREATE DATABASE IF NOT EXISTS wol_app;
+USE wol_app;
+
+-- Crear la tabla "credenciales"
+CREATE TABLE IF NOT EXISTS credenciales (
+    usuario VARCHAR(20) PRIMARY KEY,
+    hash VARCHAR(255),
+    rol VARCHAR(10)
+);
+
+-- Insertar un usuario de prueba
+INSERT INTO credenciales (usuario, hash, rol) VALUES ('test_user', '12345', 'admin');
